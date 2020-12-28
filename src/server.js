76e78de519dec1a8abb66870a7882030ca6405ea
Chris Sanders <astericky@gmail.com>
2020-12-28 13:49:21 -0500

    update route.rest and server database

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import organizationrRouter from './routes/organizations.js'
 
-mongoose.connect(process.env.DATABASE_URL_LOCAL, { useCreateIndex: true, useNewUrlParser: true,  useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL, { useCreateIndex: true, useNewUrlParser: true,  useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to Database'))
