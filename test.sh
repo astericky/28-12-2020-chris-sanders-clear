56a5bf95efac8a5744102225e07bc9a6c33a37d7
Chris Sanders <astericky@gmail.com>
2021-01-03 16:01:27 -0500

    add test docker file

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-docker-compose stop
 
-docker-compose up --build --rm
+docker-compose -f docker-compose.test.yml up --build
 
-WAIT_FOR_PG_ISREADY="while ! pg_isready --quiet; do sleep 1; done;"
-docker-compose exec mongo bash -c "$WAIT_FOR_PG_ISREADY"
+# WAIT_FOR_PG_ISREADY="while ! pg_isready --quiet; do sleep 1; done;"
+# docker-compose exec mongo bash -c "$WAIT_FOR_PG_ISREADY"
 
+# For some reason this next line doesnt run.
 npm run test
